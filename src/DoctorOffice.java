989b7ef87d4a0be2e5e0539493552987580271c4
tsadio <tsadio@montgomerycollege.edu>
2020-04-14 14:48:55 -0400

    thirt commit

---
+++ src/DoctorOffice.java
@@ -0,0 +1,14 @@
+public class DoctorOffice {
+    public static void main (String [] args) {
+        String patientName = "Stacey Manson";
+        boolean hasInsurance =  false;
+        double temp = 102;
+        int numVisits = 5;
+
+        System.out.println("Patient Details:");
+        System.out.println("Name: "+patientName);
+        System.out.println("Patient has insurance: " + hasInsurance);
+        System.out.println("Body temperature: " + temp);
+        System.out.println("Number of Visits: " + numVisits);
+    }
+}
